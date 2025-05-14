e4e031444b8201b168c628ed96d8b4b51d5dcbda
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2025-05-14 15:37:31 +0300

    cmake and ftxui working

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
-#include <cstdlib>
 #include <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 
-int main(char** argv, int argc) {
+int main(int argc, char** argv) {
     std::cout << "Hello World" << std::endl;
+    ftxui::Element document =
+        ftxui::hbox({
+                ftxui::text("left") | ftxui::border,
+                ftxui::text("center") | ftxui::border,
+                ftxui::text("right") | ftxui::border,
+                });
+    auto screen = ftxui::Screen::Create(
+            ftxui::Dimension::Full(),
+            ftxui::Dimension::Fit(document)
+            );
+    ftxui::Render(screen, document);
+    screen.Print();
 
     return EXIT_SUCCESS;
 }
