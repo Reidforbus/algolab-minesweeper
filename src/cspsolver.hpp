34b5a3cc11244160fede82b57100ed63372701e0
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2025-05-20 16:29:07 +0300

    Started working ai

---
+++ src/cspsolver.hpp
@@ -0,0 +1,80 @@
+#include <cstdint>
+#include <map>
+#include <set>
+#include <vector>
+
+namespace algolab{
+
+    struct Coord {
+        int row;
+        int col;
+
+        Coord(int r = 0, int c = 0):row(r), col(c){}
+
+        void operator +=(Coord a){
+            row += a.row;
+            col += a.col;
+        }
+
+        bool operator <(const Coord a) const{
+            if (row < a.row){
+                return true;
+            }
+            return col < a.col;
+        }
+
+        std::vector<Coord> neighbours(Coord a){
+            std::vector<Coord> result = {{0,1},{1,1},{1,0},{0,-1},{-1,0},{-1,-1},{1,-1},{-1,1}};
+            for (Coord sq : result){
+                sq += a;
+            }
+            return result;
+        }
+
+    };
+
+    class CSPAI{
+        private:
+            uint32_t height, width, mine_count;
+            std::set<Coord> mines;
+            std::map<Coord, uint32_t> opened;
+            std::vector<Coord> changed;
+            std::vector<Coord> to_open;
+            std::vector<Coord> to_flag;
+
+
+            bool in_bounds(Coord sq){
+                return (sq.row >= 0 & sq.row < height & sq.col >= 0 & sq.col < width);
+            }
+
+            bool all_free_neighbors(Coord coord){
+                // TODO
+                return false;
+            }
+
+            bool all_mined_neighbors(Coord coord){
+                // TODO
+                return false;
+            }
+
+        public:
+            CSPAI(uint32_t h, uint32_t w, uint32_t m):
+                height(h), width(w), mine_count(m){}
+
+            void update_state(std::vector<uint32_t> state){
+                for (auto j = 0;j < height; j++){
+                    for (auto i = 0;i < width; i++){
+                        Coord sq(j, i);
+                        if (mines.count(sq) | opened.count(sq)){
+                            continue;
+                        }
+                        auto val = state[(j * width) + i];
+                        if (val < 9){
+                            changed.push_back(sq);
+                            opened.insert_or_assign(sq, val);
+                        }
+                    }
+                }
+            }
+    };
+} // namespace algolab
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,46 @@
 #include "game.hpp"
 #include "tui.hpp"
+#include "cspsolver.hpp"
+#include <cstdint>
 #include <iostream>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 
-int main(int argc, char** argv) {
+void play_game(uint32_t board_height, uint32_t board_width, uint32_t mine_count){
 
     algolab::Minegame game;
-    game.new_game(16, 30, 20);
+    game.new_game(board_height, board_width, mine_count);
 
     algolab::GameTUI tui;
-    tui.new_game(16, 30);
+    tui.new_game(board_height, board_width);
+
+    algolab::CSPAI ai(board_height, board_width, mine_count);
+
 
     while (true){
         auto new_state = game.get_state();
         tui.update_state(new_state);
+        ai.update_state(new_state);
         tui.draw();
 
         uint32_t row, col;
-        std::cin >> col >> row;
+        char command = 'o';
+        std::cin >> row >> col >> command;
         if (std::cin.fail()) {
             break;
         }
 
-        game.open(row, col);
+        if (command == 'f') {
+            game.flag(row - 1, col - 1);
+        } else {
+            game.open(row - 1, col - 1);
+        }
     }
+}
+
+int main(int argc, char** argv) {
+
+    play_game(12, 12, 40);
 
     return EXIT_SUCCESS;
 }
