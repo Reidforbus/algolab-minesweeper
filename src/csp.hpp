1385dc82bf5835474f291cf2b4b33bac9205e5aa
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2025-06-07 20:51:17 +0300

    Added logic to search for subsets and supersets Supersets don't work yet so they are skipped

--- src/csp.hpp
+++ src/csp.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <deque>
 #include <memory>
 #include <set>
 #include <tuple>
@@ -14,11 +13,19 @@ namespace algolab {
 
         struct Constraint {
             bool obsolete = false;
-            std::set<Variable*> variables;
+            std::set<Variable*> initial_vars;
             int sum = -1;
-            int ones_solved = 0;
-            int zeroes_solved = 0;
             std::set<Variable*> unknown;
+
+            void print(){
+                std::cout << "Sum: " << sum << " ";
+                for (Variable* v_ptr : initial_vars){
+                    if (unknown.count(v_ptr) == 1){
+                        std::cout << "(" << v_ptr->name.row << " " << v_ptr->name.col << ") ";
+                    }
+                }
+                std::cout << std::endl;
+            }
         };
 
         struct Variable {
@@ -33,12 +40,12 @@ namespace algolab {
         std::map<var_type, Variable> variables;
         std::vector<std::unique_ptr<Constraint>> constraints;
 
-        std::deque<Constraint*> update_queue;
+        std::vector<Constraint*> update_stack;
 
         std::vector<std::tuple<var_type, int>> solved;
 
         void solve_variable(Variable& var, int value){
-            if (var.value == value) {
+            if (var.value != -1) {
                 // Somebody cooked here
                 return;
             }
@@ -49,19 +56,11 @@ namespace algolab {
             for (Constraint* ptr : var.constraints){
                 Constraint& cnstrnt = *ptr;
 
-                std::cout << cnstrnt.unknown.size() << std::endl;
-                if (cnstrnt.unknown.count(&var) > 0){
-                    cnstrnt.unknown.erase(&var);
-                }
-
-                if (value == 1){
-                    cnstrnt.ones_solved += 1;
-                } else {
-                    cnstrnt.zeroes_solved += 1;
-                }
+                cnstrnt.unknown.erase(&var);
+                cnstrnt.sum -= value;
 
                 if (cnstrnt.unknown.size() > 0){
-                    update_queue.push_back(&cnstrnt);
+                    update_stack.push_back(&cnstrnt);
                 } else {
                     cnstrnt.obsolete = true;
                 }
@@ -70,21 +69,21 @@ namespace algolab {
         }
 
         void process_queue(){
-            while (!update_queue.empty()){
-                // Grab a constraint from the queue
-                Constraint& cnstrnt = *update_queue.front();
-                update_queue.pop_front();
+            while (!update_stack.empty()){
+                // Grab a constraint from the stack
+                Constraint& cnstrnt = *update_stack.back();
+                update_stack.pop_back();
 
-                // In case the constraint has been rendered obsolete before reaching the front of the queue
+                // In case the constraint has been rendered obsolete before reaching top of stack
                 if (cnstrnt.obsolete) continue;
 
                 // If the constraint is trivial the variables are solved
                 // then the constraints that include those variables
-                // are updated and added onto the queue.
+                // are updated and added onto the stack
                 // Then the process starts over
 
                 // All variables zero
-                if (cnstrnt.ones_solved == cnstrnt.sum){
+                if (cnstrnt.sum == 0){
                     auto vars = cnstrnt.unknown;
                     for (Variable* ptr : vars){
                         Variable& var = *ptr;
@@ -95,7 +94,7 @@ namespace algolab {
                 } 
 
                 // All variables one
-                if (cnstrnt.variables.size() - cnstrnt.zeroes_solved == cnstrnt.sum){
+                if (cnstrnt.unknown.size() == cnstrnt.sum){
                     auto vars = cnstrnt.unknown;
                     for (Variable* ptr : vars){
                         Variable& var = *ptr;
@@ -108,19 +107,100 @@ namespace algolab {
                 // For constraints that are not trivial, existing constraints that share variables
                 // are checked if they are sub or supersets.
                 // Then the smaller set is cut from the larger and the changed constraint is added to queue
-                std::set<Constraint*> possible_supersets;
-                std::set<Constraint*> possible_subsets;
 
-                for (Variable* ptr : cnstrnt.unknown){
-                    Variable& var = *ptr;
-                    //TODO
+                // Subsets
+                Constraint* subset = nullptr;
+
+                for (Variable* v_ptr : cnstrnt.unknown){
+                    for (Constraint* c_ptr : v_ptr->constraints){
+                        Constraint& c = *c_ptr;
+                        if ((c.unknown.size() >= cnstrnt.unknown.size()) | (c.unknown.size() == 0)) continue;
+
+                        bool valid = true;
+                        for (Variable* v2_ptr : c.unknown){
+                            if (cnstrnt.unknown.count(v2_ptr) == 0){
+                                valid = false;
+                                break;
+                            }
+                        }
+                        if (valid){
+                            subset = &c;
+                            break;
+                        }
+                    }
+                    if (subset != nullptr){
+                        break;
+                    }
                 }
 
+                if (subset != nullptr){
 
-                //Etsi ylijoukot
-                //Etsi alijoukot
-                //Jaa löydetyt joukot osiin ja lisää jonoon
+                    std::cout << "\nFound a subset! ";
+                    subset->print();
+                    cnstrnt.print();
+                    cnstrnt.sum -= subset->sum;
+
+                    for (Variable* v_ptr : subset->unknown){
+                        cnstrnt.unknown.erase(v_ptr);
+                    }
+                    cnstrnt.print();
+                    update_stack.push_back(&cnstrnt);
+                    continue;
+                }
+
+                continue;
+                // Supersets
+                std::set<Constraint*> supersets;
+
+                // Initially all constraints that share the first variable
+                // and are larger are added to the set
+                auto vars = cnstrnt.unknown.cbegin();
+                Variable& first = **vars++;
+                for (Constraint* c_ptr : first.constraints){
+                    if (c_ptr->unknown.size() > cnstrnt.unknown.size()){
+                        if (c_ptr->unknown.count(&first) > 0) supersets.insert(c_ptr);
+                    }
+
+                }
+
+                // Then the constraint are checked for each subsequent variable
+                for (;vars != cnstrnt.unknown.cend();vars++){
+                    Variable* v_ptr = *vars;
+                    for (auto iter = supersets.begin(); iter != supersets.end();){
+                        Constraint* c_ptr = *iter;
+                        if (c_ptr->unknown.count(v_ptr) > 0){
+                            iter++;
+                        } else {
+                            iter = supersets.erase(iter);
+                        }
+                    }
+                }
+
+                // If any supersets are found, their sum is reduced and variables cut
+
+                if (supersets.size() > 0){
+                    std::cout << "\nFound a superset! ";
+                    cnstrnt.print();
+                    for (Constraint* c_ptr : supersets){
+                        Constraint& c = *c_ptr;
+                        c.print();
+
+                        c.sum  -= cnstrnt.sum;
+
+                        for (Variable* v_ptr : cnstrnt.unknown){
+                            c.unknown.erase(v_ptr);
+                        }
+
+                        c.print();
+                        update_stack.push_back(&c);
+                    }
+                    continue;
+                }
             }
+
+            //TODO: When queue is empty and no solutions are ready,
+            // move to a backtracking search for all possible solutions
+            // and find common ones and zeroes
         }
 
         public:
@@ -139,20 +219,12 @@ namespace algolab {
 
                 Variable& var = variables[var_name];
                 var.constraints.insert(new_cnstrnt);
-                new_cnstrnt->variables.insert(&var);
+                new_cnstrnt->initial_vars.insert(&var);
                 new_cnstrnt->unknown.insert(&var);
             }
 
             new_cnstrnt->sum = sum;
-            update_queue.push_back(new_cnstrnt);
-
-
-            std::cout << "New constraint added, Sum: " << new_cnstrnt->sum << ", Variables: ";
-            for (Variable* ptr : new_cnstrnt->variables){
-                Variable& var = *ptr;
-                std::cout << "(" << var.name.row << ", " << var.name.col << ") ";
-            }
-            std::cout << std::endl;
+            update_stack.push_back(new_cnstrnt);
 
             process_queue();
         }
