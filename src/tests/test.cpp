ba5b755f415d3865a71ef0e5cf02e89ad39f2b2a
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2025-06-12 17:20:40 +0300

    Started making unit tests

---
+++ src/tests/test.cpp
@@ -0,0 +1,3 @@
+#include <catch2/catch_test_macros.hpp>
+#include "test_utilities.hpp"
+#include "test_game.hpp"
---
+++ src/tests/test_game.hpp
@@ -0,0 +1,167 @@
+#include <catch2/catch_test_macros.hpp>
+#include "../game.hpp"
+
+namespace algolab{
+    class MinegameTest {
+        Minegame game;
+
+        public:
+
+        void test_in_bounds(){
+            game.new_game(10, 10, 1, 0, true);
+
+            REQUIRE(game.in_bounds(0, 0));
+            REQUIRE(game.in_bounds(9, 9));
+
+            REQUIRE_FALSE(game.in_bounds(-1,-1));
+            REQUIRE_FALSE(game.in_bounds(0,-1));
+            REQUIRE_FALSE(game.in_bounds(-1,0));
+
+            REQUIRE_FALSE(game.in_bounds(0,10));
+            REQUIRE_FALSE(game.in_bounds(10,0));
+            REQUIRE_FALSE(game.in_bounds(10,10));
+            REQUIRE_FALSE(game.in_bounds(-1,10));
+            REQUIRE_FALSE(game.in_bounds(10,-1));
+
+        }
+
+        void test_number_of_mines(int n){
+            game.new_game(10, 10, n, 0, true);
+            REQUIRE(count_mines() == n);
+        }
+
+        int count_mines(){
+            int n = 0;
+            for (auto row : game.board){
+                for (auto sq : row){
+                    if (sq.mine){
+                        n++;
+                    }
+                }
+            }
+            return n;
+        }
+
+        void check_generation(uint32_t seed, int n){
+            game.new_game(5,5,n,seed,true);
+            REQUIRE(count_mines() == n);
+
+            for (int i = 0; i < 25; i++){
+                bool mine = game.board[i/5][i%5].mine;
+                if (i < n){
+                    REQUIRE(mine);
+                } else {
+                    REQUIRE_FALSE(mine);
+                }
+            }
+        }
+
+        uint32_t find_pattern_seed(std::string s){
+            uint32_t test_seed = 0;
+            int n = 0;
+            for (char c : s){
+                n += (c == '@');
+            }
+            std::cout << "Found " << n << " mines" << std::endl;
+            while (true){
+                game.new_game(5,5,n,test_seed++ ,true);
+                bool valid = true;
+                for (int i = 0; i < 25; i++){
+                    bool mine = game.board[i/5][i%5].mine;
+                    if (mine != (s[i] == '@')) valid = false;
+                }
+                if (valid){
+                    game.print_state();
+                    return game.get_seed();
+                }
+            }
+        }
+    };
+
+    TEST_CASE("Game can be generated with valid parameters", "[Game]"){
+        Minegame game;
+        REQUIRE_NOTHROW(game.new_game(10, 10, 1, 0, true));
+        REQUIRE_NOTHROW(game.new_game(10, 10, 99, 0, true));
+    }
+
+    TEST_CASE("Game cannot be generated with faulty parameters", "[Game]"){
+        Minegame game;
+        REQUIRE_THROWS(game.new_game(10, 10, 101, 0, true));
+        REQUIRE_THROWS(game.new_game(10, 10, 100, 0, true));
+        REQUIRE_THROWS(game.new_game(10, 10, 0, 0, true));
+        REQUIRE_THROWS(game.new_game(10, 10, -1, 0, true));
+
+        REQUIRE_THROWS(game.new_game(-1, 10, 5, 0, true));
+        REQUIRE_THROWS(game.new_game(0, 10, 5, 0, true));
+
+
+        REQUIRE_THROWS(game.new_game(10, -10, 5, 0, true));
+        REQUIRE_THROWS(game.new_game(10, 0, 5, 0, true));
+    }
+
+    TEST_CASE("in_bounds works correctly", "[Game]"){
+        MinegameTest().test_in_bounds();
+    }
+
+    TEST_CASE("Correct number of mines on board", "[Game]"){
+        MinegameTest().test_number_of_mines(1);
+        MinegameTest().test_number_of_mines(50);
+        MinegameTest().test_number_of_mines(99);
+    }
+
+    TEST_CASE("Seeded board generation works expectedly", "[Game]"){
+        MinegameTest gametester;
+
+        gametester.check_generation(9, 1);
+        gametester.check_generation(729, 2);
+        gametester.check_generation(1736, 3);
+        gametester.check_generation(14763, 4);
+        gametester.check_generation(100311, 5);
+        gametester.check_generation(207577, 6);
+        gametester.check_generation(148667, 7);
+        gametester.check_generation(106828, 8);
+        gametester.check_generation(973441, 9);
+        gametester.check_generation(957779, 10);
+        gametester.check_generation(10660902, 11);
+        gametester.check_generation(6336529, 12);
+        gametester.check_generation(1200796, 13);
+        gametester.check_generation(3426934, 14);
+        gametester.check_generation(4998722, 15);
+        gametester.check_generation(118842, 16);
+        gametester.check_generation(2210161, 17);
+        gametester.check_generation(1052207, 18);
+        gametester.check_generation(35525, 19);
+        gametester.check_generation(128201, 20);
+        gametester.check_generation(26072, 21);
+        gametester.check_generation(1201, 22);
+        gametester.check_generation(69, 23);
+        gametester.check_generation(10, 24);
+
+    }
+
+    TEST_CASE("Game is over and lost when mined square is opened", "[Game]"){
+        Minegame game;
+        game.new_game(5,5,1,9,true);
+        game.open(0,0);
+        REQUIRE(game.game_finished());
+        REQUIRE_FALSE(game.was_won());
+    }
+
+    TEST_CASE("Game is not over when free square is opened", "[Game]"){
+        Minegame game;
+        game.new_game(5,5,6,30627,true);
+        game.open(4,4);
+        REQUIRE_FALSE(game.game_finished());
+        game.open(0,0);
+        REQUIRE(game.game_finished());
+    }
+
+    TEST_CASE("Game is over and won when no more unopened free squares remain", "[Game]"){
+        Minegame game;
+        game.new_game(5,5,5,100311,true);
+        game.open(4,4);
+        REQUIRE(game.game_finished());
+        REQUIRE(game.was_won());
+    }
+
+}
